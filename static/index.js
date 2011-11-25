3da22d88895fa38b288411980c1c8378f110b496
Peter Downs <peter.l.downs@gmail.com>
2011-11-25 13:39:52 -0500

    Cleans up javascript, uses $.ajax instead of $.get

--- static/index.js
+++ static/index.js
@@ -1,38 +1,38 @@
-function bg_main_on(){	
-	$("html").removeClass("bg_reader");
-	$("html").addClass("bg_main");
-}
-function bg_main_off(){
-	$("html").removeClass("bg_main");
-	$("html").addClass("bg_reader");
-}
-
 $(document).ready(function(){
 	$("#content").show(); // show the articles
 	$("#text").hide(); // don't show any one article in particular
-	bg_main_on(); // turn on the background image
 	$(".article").each(function(index, obj){
 		$(obj).click(function(e){
 			$("#content").fadeOut("fast");
-			var loadingstr = "<h1> Loading </h1><img src=\"/static/ajax-loader.gif\" />";
-			$("#pageTitle").html(loadingstr);
-			bg_main_off();
+			$("#pageTitle").html("<h3>Loading </h3><img src=\"/static/ajax-loader.gif\" />");
 			var url = $(this).attr('url');
-			$.get("/viewtext?url="+url, function(datastr){
-				var data = $.parseJSON(datastr);
-				var close = '<a class="close" href="">(close)</a>';
-				var title = '<a href="'+data.url+'">'+data.title+'</a>';
-				console.log(data);
-				$("#pageTitle").html(title).append(close);
-				$("#text").html(close+data.body+close).fadeIn("slow");
-				$(".close").each(function(index, obj){
-					$(obj).click(function(){
-						$("#text").hide().html("");
-						$("#pageTitle").html("<h1>Headliner - The Daily News</h1>");
-						bg_main_on();
-						$("#content").fadeIn("slow");
+			$.ajax({
+				url: '/viewtext?url='+url,
+				cache: true,
+				success: function(response){
+					var data = $.parseJSON(response);
+					var close = '<a class="close" href="">(close)</a>';
+					var title = '<a href="'+data.url+'">'+data.title+'</a>';
+					$("#pageTitle").html(title);
+					$("#pageTitle").append(close);
+					$("#text").html(close+data.body+close).fadeIn("fast");
+				},
+				error: function(response){
+					console.log("There was an error with the request");
+					console.log("Response:");
+					console.log(response);
+					$("#pageTitle").html("<h3>There was an error with the request");
+					$("#text").html('<center><a class="close" href="">Click here to return to the homepage</a>');
+				},
+				complete: function(){
+					$(".close").each(function(index, obj){
+						$(obj).click(function(){
+							$("#text").hide().html("");
+							$("#pageTitle").html("<h1>Headliner - The Daily News</h1>");
+							$("#content").fadeIn("fast");
+						});
 					});
-				});
+				},
 			});
 			return false;
 		});
